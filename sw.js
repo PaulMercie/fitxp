a70f1e8e472e140b4676cc27f4696a39e61a9707
PaulMercie <paulmercie453@gmail.com>
2026-05-24 18:46:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fitxp-v2';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'fitxp-v3';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -13,8 +13,15 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
+// Network-first : toujours la version en ligne, cache en fallback (hors-ligne)
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
